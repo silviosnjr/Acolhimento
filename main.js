39c71674811833c48b965142eea0c8c8b1a1613c
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2024-01-29 17:26:54 -0300

    emojis

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-alert("Olá");
+const emojis = ["😊","😁","😎","🤗","🙂","🤓","😜","😂","😄","😊","😁","😃","😎","🤗","🙂","🤓","😜","😂","😄","😃"];
+
+let elementosEmoji = documet.querySelectorAll(".emoji");
+
+for(let contador = 0; contador < elementosEmoji.length; contador++){
+    elementosEmoji[contador].textContent(emojis[contador]);
+}
