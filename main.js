1771931e84ad7cbf65bfa4fc9f251422e5680682
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2024-01-29 18:00:25 -0300

    teste

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const emojis = ["😊","😁","😎","🤗","🙂","🤓","😜","😂","😄","😊","😁","😃","😎","🤗","🙂","🤓","😜","😂","😄","😃"];
+const colors = ["lightblue","lightcoral","lightcyan","lightgoldenrodyellow","lightgray","lightgreen","lightpink","lightsalmon","lightseagreen","lightskyblue","lightslategrey","lightsteelblue","lightyellow","lightblue","lightgrey","lightsalmon","lightgoldenrodyellow","lightsteelblue","lightpink","lightskyblue"]
 
 let ElmentosPersonalize = document.querySelectorAll(".personalize");
 
 for(let contador = 0; contador < ElmentosPersonalize.length; contador++){    
+    ElmentosPersonalize[contador].computedStyleMap.backgroundColor = colors[contador];
     const elementoP = document.createElement('p');   
     elementoP.className = 'emoji';
     elementoP.innerText = emojis[contador];
