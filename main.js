004855cb85fc427a059af9be228611b75c86eef3
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2024-01-29 17:35:06 -0300

    teste

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const emojis = ["😊","😁","😎","🤗","🙂","🤓","😜","😂","😄","😊","😁","😃","😎","🤗","🙂","🤓","😜","😂","😄","😃"];
 
-let elementosEmoji = document.querySelectorAll(".emoji");
+let ElmentosPersonalize = document.querySelectorAll(".emoji");
 
-for(let contador = 0; contador < elementosEmoji.length; contador++){
-    elementosEmoji[contador].html = "<span>"+(contador+1)+"</span><p class='emoji'></p>"+emojis[contador]+"</>";
+for(let contador = 0; contador < ElmentosPersonalize.length; contador++){
+    ElmentosPersonalize[contador].insertAdjacentHTML('beforebegin', "<span>"+(contador+1)+"</span><p class='emoji'></p>"+emojis[contador]+"</>");
 }
