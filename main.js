10fec23421e108cd12b5db898967e4b4528d5f80
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2024-01-29 17:50:55 -0300

    teste

--- main.js
+++ main.js
@@ -3,5 +3,10 @@ const emojis = ["😊","😁","😎","🤗","🙂","🤓","😜","😂","😄","
 let ElmentosPersonalize = document.querySelectorAll(".personalize");
 
 for(let contador = 0; contador < ElmentosPersonalize.length; contador++){
-    ElmentosPersonalize[contador].html = "<span>"+(contador+1)+"</span><p class='emoji'></p>"+emojis[contador]+"</>"+ElmentosPersonalize[contador];
+    const elementoSpan = document.createElement('span');
+    elementoSpan.innerText = contador+1;
+    ElmentosPersonalize[contador].insertBefore(elementoSpan, ElmentosPersonalize[contador].firstChild);
+    const elementoP = document.createElement('p');   
+    elementoP.innerText = emojis[contador];
+    ElmentosPersonalize[contador].insertBefore(elementoP, ElmentosPersonalize[contador].firstChild);    
 }
